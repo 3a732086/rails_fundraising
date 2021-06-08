beeb67b499af2b534db1166fd660340ed7ac4c65
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-06-09 02:34:02 +0800

    建立資料表之間的關係以及驗證欄位

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+  has_many :projects
+
+  enum status: [:is_hidden, :is_published]
+
+  validates :name, presence: true
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,23 @@
 class Project < ApplicationRecord
+  belongs_to :project_owner
+  belongs_to :category
+  has_many :project_supports
+
+  enum status: [:is_hidden, :is_published, :succeeded, :failed, :cancel]
+
+  # mount_uploader :cover_image, CoverImageUploader
+
+
+  validates :name, :brief, :description, :ad_url, :cover_image, presence: true
+  validates_numericality_of :goal, greater_than: 0
+  validate :valid_due_date?
+
+  private
+
+  def valid_due_date?
+    if due_date.blank? || due_date < ((created_at || Time.zone.now) + 7.days)
+        errors.add(:due_date, "due_date is not correct")
+    end
+  end
+
 end
