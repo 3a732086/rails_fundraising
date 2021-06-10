ab271ff52e65d5b0db48ac8b5a37695e6722c341
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-06-11 01:39:58 +0800

    建立 Controller 商業邏輯

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
 
   def index
+    @projects = Project.is_now_on_sale
+
+    @categories = Category.is_published
   end
 end
--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,2 +1,41 @@
 class PledgesController < ApplicationController
+  #可以想成是Order 訂單頁面， 只是換一個名稱
+  # 募資網站 -> 贊助 Pledge
+  # 購物網站 -> 訂單 Order
+
+  before_action :is_login?
+  before_action :get_project_support, only: [:create]
+
+  def create
+    @pledge = Pledge.create({
+                              user: current_user,
+                              project_support: @project_support,
+                              project_name: @project_support.project_name,
+                              support_name: @project_support.name,
+                              support_price: @project_support.price,
+                              quantity: params[:quantity]
+                            })
+    redirect_to mpg_payment_path(pledge: @pledge)
+  end
+
+  private
+
+  def is_login?
+    unless current_user
+      flash[:error] = "您尚未登入"
+      redirect_to login_path
+      return
+    end
+  end
+
+  def get_project_support
+    @project_support = ProjectSupport.is_published.find_by_id(params[:id])
+
+    unless @project_support
+      flash[:alert] = "沒有此贊助方案"
+      redirect_to root_path
+      return
+    end
+  end
+
 end
