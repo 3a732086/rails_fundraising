c9f22666aa4d7e3688d8c3587e668f0e0aab575b
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-06-11 13:51:42 +0800

    enum 多加一個欄位 ， 定義 end_price 方法

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -3,12 +3,16 @@ class Pledge < ApplicationRecord
   belongs_to :project_support
   has_one :payment
 
-  enum status: [:not_paid, :paid, :canceled]
+  enum status: [:not_selected_yet, :not_paid, :paid, :canceled]
 
   validates :project_name, :support_name, presence: true
   validates_numericality_of :support_price, greater_than_or_equal_to: 0
   validates_numericality_of :quantity, greater_than_or_equal_to: 1
   validate :is_user_project_owner?
+
+  def end_price
+    support_price * quantity
+  end
 
   private
 
