456d051eabec3e570e2ef87d346143e4c13641d2
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-06-13 03:43:20 +0800

    facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable#, :omniauthable
+         :confirmable, :omniauthable, omniauth_providers: [:facebook]
 
   validates :name, presence: true
 
@@ -21,6 +21,15 @@ class User < ApplicationRecord
     end
 
     return @project_owner
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name    # assuming the user model has a name
+      user.skip_confirmation!
+    end
   end
 
 end
