6d8f6b179f9ec0133ab43e69829bf67a00bc2a8e
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-06-12 21:14:56 +0800

    新增 project supports controller

--- app/controllers/project_supports_controller.rb
+++ app/controllers/project_supports_controller.rb
@@ -1,2 +1,55 @@
 class ProjectSupportsController < ApplicationController
+  before_action :get_project_for_update
+  before_action :get_project_support, except: [:create]
+
+  def create
+    @project_support = ProjectSupport.create(
+                          project: @project,
+                          name: "未命名",
+                          description: "未填寫",
+                          price: 0
+                        )
+
+    if !@project_support.valid?
+      flash[:alert] = @project_support.errors.messages.map{|k,v| "#{t(k)}, #{v[0]}" }
+    else
+      flash[:notice] = "贊助方案新增成功"
+    end
+
+    redirect_to edit_project_path(@project)
+  end
+
+  def destroy
+    if @project_support.pledges.count > 0
+      flash[:alert] = "贊助方案: #{@project_support.name} 已經有人贊助，無法刪除"
+    else
+      @project_support.destroy!
+      flash[:notice] = "贊助方案: #{@project_support.name} 已經被刪除"
+    end
+
+    redirect_to edit_project_path(@project)
+  end
+
+
+
+  def get_project_for_update
+    @project = Project.find_by(id: params[:id], project_owner: current_user.project_owner)
+
+    unless @project
+      flash[:alert] = "沒有這個募資專案"
+      redirect_to root_path
+      return
+    end
+  end
+
+  def get_project_support
+    @project_support = @project.project_supports.find_by(id: params[:project_support_id])
+
+    unless @project_support
+      flash[:alert] = "沒有這個贊助方案"
+      redirect_to edit_project_path(@project)
+      return
+    end
+  end
+
 end
